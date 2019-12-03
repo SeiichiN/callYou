2694e7dd0c3036fbdef4df42c07e7042278010fa
SeiichiN <billie175@gmail.com>
2019-12-03 13:07:46 +0900

    ソケっと通信はできた

--- app.js
+++ app.js
@@ -4,23 +4,27 @@
 
 var http = require( 'http'),
     express = require( 'express'),
-    socketIo = require('socket.io'),
+    socket = require('socket.io'),
 
     app = express(),
     server = http.createServer( app ),
-    io = socketIo.listen( server )
+    io = socket.listen( server )
 ;
 
-io.on('call', function(msg) {
-    console.log(msg);
-    
-});
 
 app.use( express.static( __dirname + '/' ));
 
 app.get( '/', function ( req, res ) {
     res.redirect( 'index.html' );
 });
+
+io.of('/')
+  .on('connection', function ( socket ) {
+    socket.on( 'call', function ( msg ) {
+        console.log('get call!');
+        console.log( msg );
+    })
+})
 
 server.listen( 3000 );
 console.log(

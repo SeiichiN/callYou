25c1e01c868b3058e8523099748f29705dd932f1
SeiichiN <billie175@gmail.com>
2019-12-03 17:47:26 +0900

    うまくうごかない

--- app.js
+++ app.js
@@ -5,11 +5,19 @@
 var http = require( 'http'),
     express = require( 'express'),
     socket = require('socket.io'),
+    electron = require( 'electron' ),
+    BrowserWindow = electron.BrowserWindow,
 
     app = express(),
+    eleApp = electron.app,
     server = http.createServer( app ),
-    io = socket.listen( server )
+    io = socket.listen( server ),
+
+    mainWin = null,
+    urlIndex = `file://${__dirname}/call.html`
 ;
+
+
 
 
 app.use( express.static( __dirname + '/' ));
@@ -18,13 +26,25 @@ app.get( '/', function ( req, res ) {
     res.redirect( 'index.html' );
 });
 
+eleApp.on( 'window-all-closed', function() {
+    eleApp.quit();
+});
+
+eleApp.on( 'ready', function() {
+    mainWin = new BrowserWindow({
+        webPreferences: { nodeIntegration: true },
+        width: 300,
+        height: 300
+    });
+});
+
+
 io.of('/')
   .on('connection', function ( socket ) {
-    socket.on( 'call', function ( msg ) {
-        console.log('get call!');
-        console.log( msg );
-    })
-})
+      socket.on( 'call', function ( msg ) {
+          mainWin.loadURL( urlIndex );
+      });
+  });
 
 server.listen( 3000 );
 console.log(

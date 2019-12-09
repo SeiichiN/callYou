4743442807e35e390a20747fef82e2ad542e991c
SeiichiN <billie175@gmail.com>
2019-12-10 07:58:12 +0900

    まだまだやな

--- app.js
+++ app.js
@@ -4,20 +4,11 @@
 
 var http = require( 'http'),
     express = require( 'express'),
-    socket = require('socket.io'),
-    electron = require( 'electron' ),
-    BrowserWindow = electron.BrowserWindow,
 
     app = express(),
-    eleApp = electron.app,
-    server = http.createServer( app ),
-    io = socket.listen( server ),
 
-    mainWin = null,
-    urlIndex = `file://${__dirname}/call.html`
+    server = http.createServer( app )
 ;
-
-
 
 
 app.use( express.static( __dirname + '/' ));
@@ -25,26 +16,6 @@ app.use( express.static( __dirname + '/' ));
 app.get( '/', function ( req, res ) {
     res.redirect( 'index.html' );
 });
-
-eleApp.on( 'window-all-closed', function() {
-    eleApp.quit();
-});
-
-eleApp.on( 'ready', function() {
-    mainWin = new BrowserWindow({
-        webPreferences: { nodeIntegration: true },
-        width: 300,
-        height: 300
-    });
-});
-
-
-io.of('/')
-  .on('connection', function ( socket ) {
-      socket.on( 'call', function ( msg ) {
-          mainWin.loadURL( urlIndex );
-      });
-  });
 
 server.listen( 3000 );
 console.log(
---
+++ main.js
@@ -0,0 +1,37 @@
+// main.js
+
+'use strict';
+
+var electron = require( 'electron' ),
+    appExpress = require('./app'),
+    socket = require('socket.io'),
+    io = socket.listen( server ),
+
+    app = electron.app,
+
+    BrowserWindow = electron.BrowserWindow,
+    urlIndex = `file://${__dirname}/call.html`,
+
+    mainWindow;
+
+app.on( 'window-all-closed', function () {
+  if ( process.platform != 'darwin' ) {
+    app.quit();
+  }
+});
+
+app.on( 'ready', function () {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL( 'http://127.0.0.1:3000' );
+
+  mainWindow.on( 'closed', function () {
+    mainWindow = null;
+  });
+});
+
+io.of('/')
+  .on('connection', function ( socket ) {
+    socket.on( 'call', function ( msg ) {
+      mainWin.loadURL( urlIndex );
+      });
+  });

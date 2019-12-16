f2d8b34b900d77155771604486987ba9a36ea6fe
SeiichiN <billie175@gmail.com>
2019-12-17 08:06:22 +0900

    あともう少しかなあ

--- main.js
+++ main.js
@@ -3,13 +3,33 @@
 'use strict'
 
 const electron = require('electron')
-// const appExpress = require('./app')
-
 const app = electron.app
 
-cont BrowserWindow = electron.BrowserWindow
+const nodeStatic = require('node-static')
+const file = new nodeStatic.Server(__dirname)
 
-let mainWindow
+const http = require('http')
+
+const BrowserWindow = electron.BrowserWindow
+
+const socket = require('socket.io')
+
+let mainWindow = null
+
+const server = http.createServer( function (request, response) {
+  request.addListener( 'end', function() {
+    file.serve(request, response)
+  }).resume()
+})
+
+const io = socket.listen(server)
+
+io.of('/')
+                 .on('connection', function ( socket ) {
+                   socket.on( 'call', function (msg) {
+                     console.log(msg)
+                   })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform != 'darwin') {
@@ -19,9 +39,14 @@ app.on('window-all-closed', () => {
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width:800, height:600})
-  mainWindow.loadURL('http://127.0.0.1:8888')
+  mainWindow.loadURL('http://localhost:8888/index.html')
 
   mainWindow.on('closed', () => {
     mainWindow = null
   })
 })
+
+server.listen(8888)
+console.log(
+  'Server listening on port %d', server.address().port
+)

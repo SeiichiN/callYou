e5243b765c0d5e1c7b3dea2af33c97ec437889ec
SeiichiN <billie175@gmail.com>
2019-12-17 22:07:00 +0900

    とりあえず動いた

--- main.js
+++ main.js
@@ -4,9 +4,10 @@
 
 const electron = require('electron')
 const app = electron.app
+const ipcMain = electron.ipcMain
 
 const nodeStatic = require('node-static')
-const file = new nodeStatic.Server(__dirname)
+const file = new nodeStatic.Server(__dirname + '/public')
 
 const http = require('http')
 
@@ -28,6 +29,7 @@ io.of('/')
                  .on('connection', function ( socket ) {
                    socket.on( 'call', function (msg) {
                      console.log(msg)
+                     mainWindow.webContents.send('async-call', msg)
                    })
 })
 
@@ -38,8 +40,14 @@ app.on('window-all-closed', () => {
 })
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width:800, height:600})
-  mainWindow.loadURL('http://localhost:8888/index.html')
+  mainWindow = new BrowserWindow({
+    width:800,
+    height:600,
+    webPreferences : {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadURL('http://localhost:8888/call.html')
 
   mainWindow.on('closed', () => {
     mainWindow = null
@@ -50,3 +58,6 @@ server.listen(8888)
 console.log(
   'Server listening on port %d', server.address().port
 )
+
+// Electronでプロセス間通信(ipc) MainProcessからRendererProcessへ非同期通信
+// https://gist.github.com/KimuraTakaumi/9f184167f321d8f618ec
